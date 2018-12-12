b42b6716d2946f921e302e548cd84360bab37bf3
Leonid <sleonidy@gmail.com>
2018-12-12 10:23:25 +0200

    Rewrite plugin to swift

--- ios/Classes/SwiftFlutterSharePlugin.swift
+++ ios/Classes/SwiftFlutterSharePlugin.swift
@@ -1,111 +1,127 @@
 import Flutter
 import UIKit
 
-public class SwiftFlutterSharePlugin: NSObject, FlutterPlugin,UIActivityItemSource {
-    public func activityViewControllerPlaceholderItem(_ activityViewController: UIActivityViewController) -> Any {
-        return "nil"
-    }
-    
-    public func activityViewController(_ activityViewController: UIActivityViewController, itemForActivityType activityType: UIActivity.ActivityType?) -> Any? {
-        return "nil"
-    }
-    
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "com.github.sleonidy/share", binaryMessenger: registrar.messenger())
-    let instance = SwiftFlutterSharePlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+public class SwiftFlutterSharePlugin: NSObject, FlutterPlugin, UIActivityItemSource {
+    private var flutterShareParams: FlutterSharePluginParams? = nil
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-    NSLog("Inside handle")
-    switch call.method {
-    case "share":
-        guard let params = parseArgs(call, result: result) else {
-            result(FlutterError(code:"ParamsMissing",message:"Cannot parse params",details:nil))
-            break
-        }
-        share(params,result:result)
-    default:
-        result(FlutterMethodNotImplemented)
+    public func activityViewControllerPlaceholderItem(_ activityViewController: UIActivityViewController) -> Any {
+        return ""
     }
-  }
+
+    public func activityViewController(_ activityViewController: UIActivityViewController, itemForActivityType activityType: UIActivity.ActivityType?) -> Any? {
+        if (activityType == .mail) {
+            return flutterShareParams?.text
+        }
+        return nil
+    }
+
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "com.github.sleonidy/share", binaryMessenger: registrar.messenger())
+        let instance = SwiftFlutterSharePlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        result("iOS " + UIDevice.current.systemVersion)
+        NSLog("Inside handle")
+        switch call.method {
+        case "share":
+            guard let params = parseArgs(call, result: result) else {
+                result(FlutterError(code: "ParamsMissing", message: "Cannot parse params", details: nil))
+                break
+            }
+            flutterShareParams = params
+            share(params, result: result)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+
     private func parseArgs(_ call: FlutterMethodCall, result: @escaping FlutterResult) -> FlutterSharePluginParams? {
         guard let args = call.arguments as? [String: Any?] else {
             return nil
         }
-        
+
         return FlutterSharePluginParams(
-            type: args[FlutterSharePluginParams.TYPE] as? String,
-            path: args[FlutterSharePluginParams.PATH] as? String,
-            title: args[FlutterSharePluginParams.TITLE] as? String,
-            text: args[FlutterSharePluginParams.TEXT] as? String,
-            isMultiple:args[FlutterSharePluginParams.IS_MULTIPLE] as! Bool,
-            originX:args[FlutterSharePluginParams.IS_MULTIPLE] as? NSNumber,
-            originY:args[FlutterSharePluginParams.IS_MULTIPLE] as? NSNumber,
-            originHeight:args[FlutterSharePluginParams.IS_MULTIPLE] as? NSNumber,
-            originWidth:args[FlutterSharePluginParams.IS_MULTIPLE] as? NSNumber,
-            arguments:args
+                type: args[FlutterSharePluginParams.TYPE] as? String,
+                path: args[FlutterSharePluginParams.PATH] as? String,
+                title: args[FlutterSharePluginParams.TITLE] as? String,
+                text: args[FlutterSharePluginParams.TEXT] as? String,
+                isMultiple: args[FlutterSharePluginParams.IS_MULTIPLE] as! Bool,
+                originX: args[FlutterSharePluginParams.IS_MULTIPLE] as? NSNumber,
+                originY: args[FlutterSharePluginParams.IS_MULTIPLE] as? NSNumber,
+                originHeight: args[FlutterSharePluginParams.IS_MULTIPLE] as? NSNumber,
+                originWidth: args[FlutterSharePluginParams.IS_MULTIPLE] as? NSNumber,
+                arguments: args
         )
     }
-    private func share(_ params:FlutterSharePluginParams,result:@escaping FlutterResult){
+
+    private func share(_ params: FlutterSharePluginParams, result: @escaping FlutterResult) {
+        
         NSLog("Share")
+        var avc:UIActivityViewController
         switch params.type {
         case "image/*":
-            shareImages(params,controller:(UIApplication.shared.keyWindow?.rootViewController!)!, result:result)
+            avc = shareImages(params, result: result)
         case "text/plain":
-            shareTexts(params,controller:(UIApplication.shared.keyWindow?.rootViewController!)!,result:result)
+            avc = shareTexts(params, result: result)
         default:
-            shareFiles(params,controller:(UIApplication.shared.keyWindow?.rootViewController!)!,result:result)
-            
+            avc = shareFiles(params, result: result)
+
         }
+        avc.setValue(params.title, forKey: "subject")
+        UIApplication.shared.keyWindow?.rootViewController!.present(avc, animated: true, completion: nil)
     }
-    private func shareImages(_ params:FlutterSharePluginParams,controller:UIViewController,result:@escaping FlutterResult){
-        var arrayOfImages:Array<UIImage?>  = Array<UIImage?>()
-        if params.isMultiple{
+
+    private func shareImages(_ params: FlutterSharePluginParams,  result: @escaping FlutterResult) ->UIActivityViewController {
+        var array: Array<Any?> = Array<Any?>()
+        if params.isMultiple {
             var i = 0
-            while params.arguments[String(i)] != nil{
-                arrayOfImages.append(UIImage(contentsOfFile: params.arguments[String(i)] as! String))
-                i = i+1
+            while params.arguments[String(i)] != nil {
+                array.append(UIImage(contentsOfFile: params.arguments[String(i)] as! String))
+                i = i + 1
             }
-        }else {
-            arrayOfImages.append(UIImage(contentsOfFile: params.path!))
+        } else {
+            array.append(UIImage(contentsOfFile: params.path!))
         }
-        
-        let avc = UIActivityViewController(activityItems: arrayOfImages as [Any], applicationActivities: nil)
-        controller.present(avc, animated: true, completion: nil)
+        array.append(self)
+        let avc = UIActivityViewController(activityItems: array as [Any], applicationActivities: nil)
+        return avc
     }
-    private func shareTexts(_ params:FlutterSharePluginParams,controller:UIViewController,result:@escaping FlutterResult){
-        var arrayOfImages:Array<String?>  = Array<String?>()
-        if params.isMultiple{
+
+    private func shareTexts(_ params: FlutterSharePluginParams,  result: @escaping FlutterResult) ->UIActivityViewController {
+        var array: Array<Any> = Array<Any>()
+        if params.isMultiple {
             var i = 0
-            while params.arguments[String(i)] != nil{
-                arrayOfImages.append(params.arguments[String(i)] as? String)
-                i = i+1
+            while params.arguments[String(i)] != nil {
+                array.append(params.arguments[String(i)] as? String ?? "")
+                i = i + 1
             }
-        }else {
-            arrayOfImages.append(params.text)
+        } else {
+            array.append(params.text ?? "")
         }
-        
-        let avc = UIActivityViewController(activityItems: arrayOfImages as [Any], applicationActivities: nil)
-        controller.present(avc, animated: true, completion: nil)
+        array.append(self)
+        let avc = UIActivityViewController(activityItems: array as [Any], applicationActivities: nil)
+        return avc
     }
-    private func shareFiles(_ params:FlutterSharePluginParams,controller:UIViewController,result:@escaping FlutterResult){
-        var arrayOfImages:Array<URL?>  = Array<URL?>()
-        if params.isMultiple{
+
+    private func shareFiles(_ params: FlutterSharePluginParams,  result: @escaping FlutterResult)->UIActivityViewController  {
+        var array: Array<Any?> = Array<Any?>()
+        if params.isMultiple {
             var i = 0
-            while params.arguments[String(i)] != nil{
-                arrayOfImages.append(URL(fileURLWithPath: params.arguments[String(i)] as! String))
-                i = i+1
+            while params.arguments[String(i)] != nil {
+                array.append(URL(fileURLWithPath: params.arguments[String(i)] as! String))
+                i = i + 1
             }
-        }else {
-            arrayOfImages.append(URL(fileURLWithPath: params.path!))
+        } else {
+            array.append(URL(fileURLWithPath: params.path!))
         }
-        
-        let avc = UIActivityViewController(activityItems: arrayOfImages as [Any], applicationActivities: nil)
-        controller.present(avc, animated: true, completion: nil)
+        array.append(self)
+        let avc = UIActivityViewController(activityItems: array as [Any], applicationActivities: nil)
+        return avc
     }
 }
+
 struct FlutterSharePluginParams {
     static let TYPE = "type"
     static let PATH = "path"
@@ -116,14 +132,14 @@ struct FlutterSharePluginParams {
     static let ORIGIN_Y = "originY"
     static let ORIGIN_HEIGHT = "originHeight"
     static let ORIGIN_WIDTH = "originWidth"
-    let type:String?
-    let path:String?
-    let title:String?
-    let text:String?
-    let isMultiple:Bool
-    let originX:NSNumber?
-    let originY:NSNumber?
-    let originHeight:NSNumber?
-    let originWidth:NSNumber?
-    let arguments:[String:Any?]
+    let type: String?
+    let path: String?
+    let title: String?
+    let text: String?
+    let isMultiple: Bool
+    let originX: NSNumber?
+    let originY: NSNumber?
+    let originHeight: NSNumber?
+    let originWidth: NSNumber?
+    let arguments: [String: Any?]
 }
